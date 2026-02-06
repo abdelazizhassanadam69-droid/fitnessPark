7d5950b2749ea3575f2b72dee432bb9d50a5f882
abdelazizhassanadam69-droid <abdelazizhassanadam69@gmail.com>
2026-02-06 11:01:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Toggle mode sombre
+const toggle = document.getElementById("darkModeToggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  toggle.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+});
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("
